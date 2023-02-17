ec866363ee20aff0ec4597fc24660069c2214c28
CeKevser <125197851+CeKevser@users.noreply.github.com>
2023-02-17 11:52:34 +0300

    Update odev11.sql

--- odev11.sql
+++ odev11.sql
@@ -1,3 +1,5 @@
+--PatikaDev SQL Ödev 11
+
 --1)actor ve customer tablolarında bulunan
 --first_name sütunları için tüm verileri sıralayalım.
 (SELECT first_name FROM actor ) UNION (SELECT first_name FROM customer);
